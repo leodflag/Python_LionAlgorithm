b07e55949aea94c33ad63bfb8a9b18828571e58a
leodflag <lovedoglion5@gmail.com>
2019-10-19 01:13:20 +0800

    The mating function is completed, and multiple lion groups can be generated.

--- lion_algoritm.py
+++ lion_algoritm.py
@@ -17,9 +17,8 @@ itera=50  # 迭代次數
 mutation_rate=0.1  # 突變率
 male_group=[] # 公獅群母體
 female_group=[] # 母獅群母體
-male_cubs_group=[] # 公幼獅群
-female_cubs_group=[] # 母幼獅群
 cubs_group=[] # 交配後的幼獅群
+lion_group_ALL=[] # 總獅群組
 adept=[] #適應值
 # 基因範圍判斷
 lion_gene1_U=255
@@ -128,18 +127,19 @@ def lion_mating(gene_1,gene_2):
     # 隨機單點交配，公母交配產生幼獅
     i=0
     cut=0
-    gene1=gene_1 # 公獅群
-    gene2=gene_2 # 母獅群
+    gene1=[] # 公獅群
+    gene2=[] # 母獅群
     while(cut < pop_lion_num): # 假設有6對
         dot=random.randint(0,7) # 隨機取交換點的位置
-        gene1[cut]=gene_1[cut][0:dot]+gene_2[cut][dot:8] # 公獅1的前段基因跟母獅1的後段基因結合
-        gene1[cut+1]=gene_2[cut][0:dot]+gene_1[cut][dot:8] # 母獅1的前段基因跟公獅1的後段基因結合
-        gene2[cut]=gene_1[cut+1][0:dot]+gene_2[cut+1][dot:8] # 公獅2的前段基因跟母獅2的後段基因結合
-        gene2[cut+1]=gene_2[cut+1][0:dot]+gene_1[cut+1][dot:8] # 母獅2的前段基因跟公獅2的後段基因結合
+        gene1.append(gene_1[cut][0:dot]+gene_2[cut][dot:8]) # 公獅1的前段基因跟母獅1的後段基因結合 一對只交配一次產生2後代
+        gene1.append(gene_2[cut][0:dot]+gene_1[cut][dot:8]) # 母獅1的前段基因跟公獅1的後段基因結合
+        
+        gene2.append(gene_1[cut+1][0:dot]+gene_2[cut+1][dot:8]) # 公獅2的前段基因跟母獅2的後段基因結合
+        gene2.append(gene_2[cut+1][0:dot]+gene_1[cut+1][dot:8]) # 母獅2的前段基因跟公獅2的後段基因結合
         cut+=2
 
     #隨機單點突變，突變部分基因
-    s1=gene_1+gene_2 # 幼獅群
+    s1=gene1+gene2 # 幼獅群
     s2=copy.deepcopy(s1) # 2n隻幼獅複製產生新2n隻幼獅
     for i in range(pop_lion_num*2):
         rand = random.random()
@@ -168,8 +168,55 @@ def lion_mating(gene_1,gene_2):
     print('male_cubs_group',male_cubs_group_1)
     print('female_cubs_group',female_cubs_group_1)
     return male_cubs_group_1,female_cubs_group_1
-
+# 交配：含隨機點交配，基因突變，k_meas分群
+def lion_mating_1(gene_1,gene_2):
+    # 隨機單點交配，公母交配產生幼獅
+    i=0
+    cut=0
+    gene_ALL=[] # 多獅群組
+    while(cut < pop_lion_num): # 假設有6對
+        gene1=[] # 交配後代基因
+        for i in range(2):
+            dot=random.randint(0,7) # 隨機取交換點的位置
+            gene1.append(gene_1[cut][0:dot]+gene_2[cut][dot:8]) # 公獅1的前段基因跟母獅1的後段基因結合 一對只交配一次產生2後代
+            gene1.append(gene_2[cut][0:dot]+gene_1[cut][dot:8]) # 母獅1的前段基因跟公獅1的後段基因結合
+        #隨機單點突變，突變部分基因
+        s1=gene1 # 幼獅群
+        s2=copy.deepcopy(s1) # 2n隻幼獅複製產生新2n隻幼獅
+        #print('s2_len',len(s2))
+        for i in range(4):
+            rand = random.random()
+            if rand <= mutation_rate: # 若隨機數小於突變率
+                h=random.randint(0,7) # 隨機位置突變，輸入為2位元，輸出字串
+                s2[i]=_invert_at(s2[i],h) # 將基因碼突變
+        s1=s1+s2 # 整合成 1 list
+        s2_int=[]
+        for i in range(8): # 為了使用kmeans，做成n*1 list
+            s1_int=[]
+            s1_int.append(bin_Int(s1[i])) # 換回十位數
+            s2_int.append(s1_int)
+        #print(s2_int)
+        X=np.array(s2_int) # list 轉 array，逗號會消失，但可直接丟到kmeans function做分類
+        X=kmeans_clusters(X) # 輸入至kmeans function做分類，輸出分類結果
+        #print(X)    
+        male_cubs_group=[] # 公幼獅群
+        female_cubs_group=[] # 母幼獅群
+        for i in range(8):
+            if X[i]==1: # 依照分類結果分公幼獅群和母幼獅群
+                male_cubs_group.append(s2_int[i][0])
+            else:
+                female_cubs_group.append(s2_int[i][0])
+        print('male_cubs_group',male_cubs_group)
+        male_cubs_group_1,female_cubs_group_1=comparison_size(male_cubs_group,female_cubs_group)  
+        gene_group=[] # 一公一母與孩子的獅群
+        gene_group.append(gene_1[cut]) # 加入交配的一隻公獅
+        gene_group.append(gene_2[cut]) # 加入交配的一隻母獅
+        gene_group.append(male_cubs_group_1) # 加入公幼獅群
+        gene_group.append(female_cubs_group_1) # 加入母幼獅群
+        gene_ALL.append(gene_group) # 成為獅群組裡的其中一個獅群
+        cut+=1
+    return gene_ALL
 #---------1.產生獅群---------
 male_group,female_group=init_lion_gene(male_group,female_group,pop_lion_num)
 #---------2.繁衍後代---------
-male_cubs_group,female_cubs_group = lion_mating(male_group,female_group)
+lion_group_ALL = lion_mating_1(male_group,female_group)
